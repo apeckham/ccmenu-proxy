e10b79582e7cdfba65e67314d534c8f38c130b39
Aaron Peckham <apeckham@gmail.com>
2010-09-12 10:17:19 -0700

    refactor

--- proxy_app_test.rb
+++ proxy_app_test.rb
@@ -3,9 +3,7 @@ require File.dirname(__FILE__) + '/proxy_app'
 
 class MyTest < Test::Unit::TestCase
   def test_truth
-    env = {"PATH_INFO" => "/cc.xml"}
-    
-    status, headers, body = ProxyApp.new.call(env)
+    status, headers, body = ProxyApp.new.call({"PATH_INFO" => "/cc.xml"})
     assert_equal 200, status
     assert_match %r[<Projects>.+</Projects>], body
   end
